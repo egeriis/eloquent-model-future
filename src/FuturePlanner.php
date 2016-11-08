1a6da7e3d1afb52e771c78623cc65e8edc4ea667
jstoone <jstoone@drk.sh>
2016-11-08 19:09:23 +0100

    Renamed variable futureQuery to futures, since the other one was awkward

--- src/FuturePlanner.php
+++ src/FuturePlanner.php
@@ -13,12 +13,12 @@ class FuturePlanner
 
     protected $attributes;
 
-    protected $futureQuery;
+    protected $futures;
 
     public function __construct(ModelFuture $model)
     {
         $this->model = $model;
-        $this->futureQuery = $this->model->uncommittedFutures();
+        $this->futures = $this->model->uncommittedFutures();
     }
 
     public function plan(array $attributes)
@@ -44,32 +44,32 @@ class FuturePlanner
 
     public function hasAnyPlans()
     {
-        return (bool) $this->futureQuery->count();
+        return (bool) $this->futures->count();
     }
 
     public function hasAnyPlansFor(Carbon $futureDate)
     {
-        return (bool) $this->futureQuery->forDate($futureDate)->count();
+        return (bool) $this->futures->forDate($futureDate)->count();
     }
 
     public function hasAnyPlansUntil(Carbon $futureDate)
     {
-        return (bool) $this->futureQuery->untilDate($futureDate)->count();
+        return (bool) $this->futures->untilDate($futureDate)->count();
     }
 
     public function getPlans()
     {
-        return $this->futureQuery->get();
+        return $this->futures->get();
     }
 
     public function getPlansFor(Carbon $futureDate)
     {
-        return $this->futureQuery->forDate($futureDate)->get();
+        return $this->futures->forDate($futureDate)->get();
     }
 
     public function getPlansUntil(Carbon $futureDate)
     {
-        return $this->futureQuery->untilDate($futureDate)->get();
+        return $this->futures->untilDate($futureDate)->get();
     }
 
 }

2fbfcc63f42f66ec0da8c49fcecc0d04490b7068
jstoone <jstoone@drk.sh>
2016-11-09 17:44:32 +0100

    Added indexes to columns.

--- migrations/2014_10_12_000000_create_futures_table.php
+++ migrations/2014_10_12_000000_create_futures_table.php
@@ -15,13 +15,15 @@ class CreateFuturesTable extends Migration
     {
         Schema::create('futures', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('futureable_id');
-            $table->string('futureable_type');
+            $table->integer('createe_user_id')->unsinged()->nullable();
+            $table->morphs('futureable');
             $table->json('data');
             $table->timestamp('commit_at');
             $table->timestamp('committed_at')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
+
+            $table->index(['committed_at', 'commit_at'], 'committed_at_and_commit_at');
         });
     }
 

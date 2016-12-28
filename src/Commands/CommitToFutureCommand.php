eaaff3a29144bd51364458c7edc2321c296fe81d
jstoone <jstoone@drk.sh>
2016-12-28 20:33:02 +0100

    Removed unnecessary constructor.

--- src/Commands/CommitToFutureCommand.php
+++ src/Commands/CommitToFutureCommand.php
@@ -23,16 +23,6 @@ class CommitToFutureCommand extends Command
     protected $description = 'A command to automatically commit future plans.';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed

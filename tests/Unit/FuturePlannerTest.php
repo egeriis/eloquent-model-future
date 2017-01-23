68089197b5128fbc7304c566c74c858add4ad350
Ronni Egeriis Persson <ronni@egeriis.me>
2017-01-23 13:39:51 +0100

    Fix spelling

--- tests/Unit/FuturePlannerTest.php
+++ tests/Unit/FuturePlannerTest.php
@@ -89,12 +89,10 @@ class FuturePlannerTest extends TestCase
         $tomorrow = Carbon::now()->addDay();
         $nextWeek = Carbon::now()->addWeek();
         $nextMonth = Carbon::now()->addMonth();
-        $previusMonth = Carbon::now()->subMonth();
-
+        $previousMonth = Carbon::now()->subMonth();
 
         $future = $this->createFuturePlanFor($user, $nextWeek);
         $future = $this->createFuturePlanFor($user, $nextMonth);
-        $future = $this->createFuturePlanFor($user, $previusMonth);
 
         $hasPlansForTomorrow = $user->future()->hasAnyPlansUntil($tomorrow);
         $hasPlansForNextMonth = $user->future()->hasAnyPlansUntil($nextMonth);
